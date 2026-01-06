a19c7514b398b15588b6db9edda32a40a7f3e49a
Dhananjay <f20240506@goa.bits-pilani.ac.in>
2026-01-06 22:19:43 +0530

    Reverting back to original status

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -76,7 +76,7 @@ router.post('/enter-phone', async (req, res) => {
   const userId = String(req.user.id);
   const phoneRegex = /^[0-9]{10}$/;
   if (!phoneRegex.test(phone_number)) {
-    return res.status(400).send('Abbey Chutiya hai kya');
+    return res.status(400).send('Invalid phone number format');
   }
 
   const { error } = await supabaseAdmin
